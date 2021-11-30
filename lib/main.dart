50f07637ceaa50168bc4dbfd655f627a9f78c24a
Genialngash <ngangadanche@gmail.com>
2021-12-01 00:00:01 +0300

    Neumorphic app bar initial design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const NeumorphicApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ngash Portfolio',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
+      themeMode: ThemeMode.dark,
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xffC4E9DB),
+        lightSource: LightSource.topLeft,
+        depth: 10,
       ),
-      home: const MyHomePage(),
+      darkTheme: NeumorphicThemeData(
+        baseColor: Color(0xFF121312),
+        lightSource: LightSource.topLeft,
+        shadowDarkColor: NeumorphicColors.decorationMaxDarkColor,
+        depth: 9,
+      ),
+      home: MyHomePage(),
     );
   }
 }
@@ -30,51 +42,151 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor:const Color(0xff0B0F0C),
+      // backgroundColor: const Color(0xff0B0F0C),
       extendBody: true,
       body: CustomScrollView(shrinkWrap: true, slivers: <Widget>[
         SliverAppBar(
-          title: const Text("Danche Ng'ang'a"),
-          backgroundColor: Colors.cyan,
+          titleSpacing: 15,
+          centerTitle: true,
+          leading: Container(
+            width: 100,
+            color: Colors.transparent,
+          ),
+          title: NeumorphicText(
+            "DANCHE   NG'ANG'A",
+            style: const NeumorphicStyle(
+              intensity: 8,
+              depth: 1.2,
+              color: Colors.white12,
+            ),
+            textStyle: NeumorphicTextStyle(
+              fontSize: 28,
+            ),
+          ),
+          backgroundColor: Colors.transparent.withOpacity(0.1),
           // expandedHeight: 200,
           // make the app bar reappear when scrolling up
           floating: true,
           //  pinned: true,
           actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search_outlined,
-                size: 30,
-                semanticLabel: 'Search',
-              ),
+            Container(
+              width: screenWidth * .15,
+              color: Colors.transparent.withOpacity(0.15),
             ),
           ],
         ),
         SliverPersistentHeader(
           delegate: PersistentHeaderDelegate(),
-         // pinned: true,
+          // pinned: true,
           // floating: true,
         ),
-        SliverList(delegate: SliverChildListDelegate([
-Container(height: 20,width: 40,color: Colors.red),
-Container(height: 20,width: 40,color: Colors.white),
-Container(height: 20,width: 40,color: Colors.blue)
-
-        ])),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              NeumorphicButton(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                onPressed: () {
+                  print("onClick");
+                },
+                style: NeumorphicStyle(
+                  lightSource: LightSource.bottom,
+                  intensity: 0.5,
+                  depth: 3,
+                  shape: NeumorphicShape.concave,
+                  boxShape: NeumorphicBoxShape.roundRect(BorderRadius.only(
+                      bottomRight: Radius.circular(10),
+                      topLeft: Radius.circular(10))),
+                ),
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  height: 150,
+                  width: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 140,
+                        child: Text(
+                          'I',
+                          style: TextStyle(fontSize: 108),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          AnimatedTextKit(
+                            animatedTexts: [
+                              WavyAnimatedText('Hello World !!',
+                                  textStyle: TextStyle(
+                                    fontSize: 18,
+                                  )),
+                              WavyAnimatedText("I'm Danche Ng'ang'a"),
+                            ],
+                            isRepeatingAnimation: false,
+                            onTap: () {
+                              print("Tap Event");
+                            },
+                          ),
+                          AnimatedTextKit(
+                            animatedTexts: [
+                              TypewriterAnimatedText(
+                                  'Discipline is the best tool'),
+                            ],
+                            onTap: () {
+                              print("Tap Event");
+                            },
+                          ),
+                          Text('I craft Awesome tech products'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 390,
+                width: 100,
+              ),
+              NeumorphicText(
+                "I love flutter",
+                style: const NeumorphicStyle(
+                  depth: 7, //customize depth here
+                  color: Colors.black, //customize color here
+                ),
+                textStyle: NeumorphicTextStyle(
+                  fontSize: 28, //customize size here
+                  // AND others usual text style properties (fontFamily, fontWeight, ...)
+                ),
+              ),
+            ],
+          ),
+        ),
       ]),
     );
   }
 }
+
+class Neumorphic {}
 
 class PersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Container(
-      height: 40,
-      color: Colors.purple,
+      height: 30,
+      decoration: BoxDecoration(
+        color: Colors.transparent.withOpacity(0.3),
+        image: const DecorationImage(
+          opacity: 0.4,
+          filterQuality: FilterQuality.medium,
+          fit: BoxFit.none,
+          image: AssetImage('assets/images/galaxy.jpg'),
+        ),
+        borderRadius:
+            const BorderRadius.only(bottomRight: Radius.elliptical(10, 20)),
+      ),
     );
   }
 
