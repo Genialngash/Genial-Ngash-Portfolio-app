7d73f7a4b841b503a560329d512f01b4a2603d67
Genialngash <ngangadanche@gmail.com>
2021-12-04 21:16:47 +0300

    the animated colorized text widget refactored

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,14 +1,18 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 Widget skillWidget({required String skill, required IconData icondata}) {
   return SizedBox(
     height: 35,
     child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Icon(
           icondata,
           color: Colors.orangeAccent,
+        ),
+        SizedBox(
+          width: 10,
         ),
         Text(
           skill,
@@ -18,5 +22,31 @@ Widget skillWidget({required String skill, required IconData icondata}) {
         )
       ],
     ),
+  );
+}
+
+Widget textAnimateColorized({required String text}) {
+  const colorizeColors = [
+    Colors.greenAccent,
+    Colors.tealAccent,
+    Colors.red,
+    Colors.white
+  ];
+  return AnimatedTextKit(
+    //TODO repeatForever: true,
+    animatedTexts: [
+      ColorizeAnimatedText(
+        text,
+        speed: Duration(milliseconds: 357),
+        textStyle: TextStyle(
+          fontSize: 30,
+        ),
+        colors: colorizeColors,
+      ),
+    ],
+    isRepeatingAnimation: true,
+    onTap: () {
+      print("Tap Event");
+    },
   );
 }
