cd3ec3a64ebd6a45ffdd8877ffe17a5b7b393b75
Genialngash <ngangadanche@gmail.com>
2021-12-04 21:16:17 +0300

    height reduced - the alignment in x axis modified - column for the project title and description

--- lib/projectsTile.dart
+++ lib/projectsTile.dart
@@ -2,32 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:timeline_tile/timeline_tile.dart' as timelineTile;
 
 class ProjectsTile extends StatelessWidget {
-  const ProjectsTile({
-    Key? key,
-    required this.screenWidth,
-  }) : super(key: key);
+  const ProjectsTile(
+      {Key? key,
+      required this.screenWidth,
+      required this.projectTitle,
+      this.projectDescription})
+      : super(key: key);
 
   final double screenWidth;
+  final String projectTitle;
+  final String? projectDescription;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-    height: 200,
-    child: timelineTile.TimelineTile(
-      
-      alignment: timelineTile.TimelineAlign.center,
-      indicatorStyle: timelineTile.IndicatorStyle(
-          width: screenWidth*0.8,
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      height: 135,
+      child: timelineTile.TimelineTile(
+        lineXY: 0.42469,
+        alignment: timelineTile.TimelineAlign.manual,
+        indicatorStyle: timelineTile.IndicatorStyle(
+          width: screenWidth * 0.8,
           height: 70,
           drawGap: true,
           indicator: Container(
-            width: screenWidth*.8,
+            width: screenWidth * .8,
             height: 10,
             decoration: BoxDecoration(
+              border: Border.all(
                 color: Colors.amber,
-                borderRadius: BorderRadius.circular(20)),
-          )),
-    ),
-                );
+              ),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Text(projectTitle),
+                Text(projectDescription ?? ' '),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
