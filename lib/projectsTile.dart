e910d96db924c376e1eca9e6a556a80a166c6d6b
Genialngash <ngangadanche@gmail.com>
2021-12-05 01:10:34 +0300

    show dialog on tap the image make the projects title clickable

--- lib/projectsTile.dart
+++ lib/projectsTile.dart
@@ -1,33 +1,74 @@
+import 'dart:developer';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:timeline_tile/timeline_tile.dart' as timelineTile;
+import 'package:url_launcher/url_launcher.dart';
 
 class ProjectsTile extends StatelessWidget {
   const ProjectsTile(
       {Key? key,
-      required this.screenWidth,
       required this.projectTitle,
+      required this.assetImageName,
+      this.url,
       this.projectDescription})
       : super(key: key);
 
-  final double screenWidth;
   final String projectTitle;
+  final String? url;
   final String? projectDescription;
+  final String assetImageName;
 
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20),
       height: 135,
       child: timelineTile.TimelineTile(
         lineXY: 0.42469,
         alignment: timelineTile.TimelineAlign.manual,
+        endChild: GestureDetector(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => AnimatedContainer(
+                height: screenHeight * .7,
+                width: screenWidth * .6,
+                duration: Duration(milliseconds: 200),
+                margin: EdgeInsets.symmetric(
+                    vertical: 40, horizontal: (screenWidth * .3).toDouble()),
+                padding: const EdgeInsets.only(top: 4),
+                decoration: BoxDecoration(
+                  backgroundBlendMode: BlendMode.softLight,
+                  color: Color(0xff000A18),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/$assetImageName'),
+                      fit: BoxFit.contain),
+                ),
+              ),
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.only(top: 4),
+            decoration: BoxDecoration(
+              backgroundBlendMode: BlendMode.softLight,
+              color: Colors.transparent.withOpacity(0.5),
+              image: DecorationImage(
+                  image: AssetImage('assets/images/$assetImageName'),
+                  fit: BoxFit.contain),
+            ),
+          ),
+        ),
         indicatorStyle: timelineTile.IndicatorStyle(
           width: screenWidth * 0.8,
-          height: 70,
+          height: 76,
           drawGap: true,
           indicator: Container(
             width: screenWidth * .8,
             height: 10,
+            padding: const EdgeInsets.all(10),
             decoration: BoxDecoration(
               border: Border.all(
                 color: Colors.amber,
@@ -36,7 +77,23 @@ class ProjectsTile extends StatelessWidget {
             ),
             child: Column(
               children: [
-                Text(projectTitle),
+                RichText(
+                  textScaleFactor: 1.3,
+                  text: TextSpan(
+                    text: projectTitle,
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        log('click');
+                        if (await canLaunch(url!)) {
+                          await launch(url!);
+                        } else {
+                          throw 'cant launch $url';
+                        }
+                      },
+                    style: const TextStyle(
+                        color: Colors.lightBlue, fontWeight: FontWeight.bold),
+                  ),
+                ),
                 Text(projectDescription ?? ' '),
               ],
             ),
