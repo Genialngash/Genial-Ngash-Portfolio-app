c0b3d42454107f76b23f2067d30f5cb216667821
Genialngash <ngangadanche@gmail.com>
2021-12-16 16:05:52 +0300

    - animated dialog for images - tooltip for when user hoovers on the image - make the whole projet tile CliCkable

--- lib/projectsTile.dart
+++ lib/projectsTile.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:timeline_tile/timeline_tile.dart' as timelineTile;
 import 'package:url_launcher/url_launcher.dart';
@@ -27,7 +28,7 @@ class ProjectsTile extends StatelessWidget {
     var screenWidth = MediaQuery.of(context).size.width;
     var screenHeight = MediaQuery.of(context).size.height;
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20),
+      margin: const EdgeInsets.only(left: 20, right: 30),
       height: 125,
       width: screenWidth,
       child: timelineTile.TimelineTile(
@@ -51,6 +52,7 @@ class ProjectsTile extends StatelessWidget {
               padding: const EdgeInsets.all(2),
               alignment: Alignment.center,
               decoration: BoxDecoration(
+                color: Colors.transparent.withOpacity(0.8),
                 borderRadius: const BorderRadius.all(Radius.elliptical(9, 3)),
                 border: Border.all(
                   width: 0.8,
@@ -67,47 +69,74 @@ class ProjectsTile extends StatelessWidget {
           ),
         ),
         endChild: InkWell(
+          borderRadius: BorderRadius.circular(17),
+          hoverColor: Colors.transparent.withOpacity(0.6),
+          splashColor: const Color(0xff604C4C),
           onTap: () {
-            log('clicked');
-          },
-          onHover: (isHovering) {
-            if (isHovering)
-              showDialog(
-                context: context,
-                builder: (context) => AnimatedContainer(
-                  height: screenHeight * .7,
-                  width: screenWidth * .6,
-                  duration: const Duration(milliseconds: 200),
-                  margin: EdgeInsets.symmetric(
-                      vertical: 40, horizontal: (screenWidth * .3).toDouble()),
-                  padding: const EdgeInsets.only(top: 4),
-                  decoration: BoxDecoration(
-                    backgroundBlendMode: BlendMode.softLight,
-                    color: const Color(0xff000A18),
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/$assetImageName'),
-                        fit: BoxFit.contain),
-                  ),
+            // if (isHovering) {
+            showAnimatedDialog(
+              duration: const Duration(milliseconds: 950),
+              barrierDismissible: true,
+              animationType: DialogTransitionType.slideFromLeftFade,
+              curve: Curves.easeInOutSine,
+              context: context,
+              builder: (context) => AnimatedContainer(
+                height: screenHeight * .7,
+                width: screenWidth * .6,
+                duration: const Duration(milliseconds: 300),
+                margin: EdgeInsets.symmetric(
+                    vertical: 40, horizontal: (screenWidth * .35).toDouble()),
+                padding: const EdgeInsets.only(top: 4),
+                decoration: BoxDecoration(
+                  backgroundBlendMode: BlendMode.softLight,
+                  color: const Color(0xff000A18),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/$assetImageName'),
+                      fit: BoxFit.contain),
                 ),
-              ).then((value) => Navigator.of(context).maybePop());
-            else {
-             // Navigator.of(context).maybePop();
+              ),
+            ).then((value) => Navigator.of(context).maybePop());
+            // } else {
+            //   // Navigator.of(context).maybePop();
 
-              print(isHovering);
-            }
+            //   print(isHovering);
+            // }
           },
-          child: Container(
-            clipBehavior: Clip.hardEdge,
-            margin: const EdgeInsets.only(top: 10, left: 7),
+          child: Tooltip(
+            message: 'Click for full image',
+            height: 17,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.elliptical(3, 13)),
-              backgroundBlendMode: BlendMode.softLight,
-              color: Colors.transparent.withOpacity(0.6),
-              image: DecorationImage(
-                  filterQuality: FilterQuality.high,
-                  opacity: 0.7,
-                  image: AssetImage('assets/images/$assetImageName'),
-                  fit: BoxFit.cover),
+                color: const Color(0xff222928),
+                borderRadius: BorderRadius.all(Radius.elliptical(3, 13)),
+                boxShadow: [
+                  // BoxShadow(
+
+                  //     color: Color(0xFF6FC3C0),
+                  //     offset: Offset(4.0, 5.0),
+                  //     blurRadius: 2.5,
+                  //     spreadRadius: .2),
+                  BoxShadow(
+                      // color: Color(0xFF5ecdff),
+                      offset: Offset(-4.0, -5.0),
+                      blurRadius: 2.5,
+                      spreadRadius: .2),
+                ]),
+            textStyle: GoogleFonts.ibmPlexMono(color: Colors.white
+                //  color: const Color(0xff222928),
+                ),
+            child: Container(
+              clipBehavior: Clip.hardEdge,
+              margin: const EdgeInsets.only(top: 10, left: 7),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.elliptical(3, 13)),
+                backgroundBlendMode: BlendMode.softLight,
+                color: Colors.transparent.withOpacity(0.6),
+                image: DecorationImage(
+                    filterQuality: FilterQuality.high,
+                    opacity: 0.7,
+                    image: AssetImage('assets/images/$assetImageName'),
+                    fit: BoxFit.cover),
+              ),
             ),
           ),
         ),
@@ -115,47 +144,60 @@ class ProjectsTile extends StatelessWidget {
           width: screenWidth * 0.68,
           height: 87,
           drawGap: true,
-          indicator: Container(
-            width: screenWidth * .8,
-            height: 10,
-            padding: const EdgeInsets.all(7),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1,
-                color: Colors.amber.withOpacity(0.3),
+          indicator: InkWell(
+            borderRadius: BorderRadius.circular(17),
+            hoverColor: Colors.transparent.withOpacity(0.6),
+            splashColor: Color(0xff604C4C),
+            onTap: () async {
+              log('click');
+              if (await canLaunch(url!)) {
+                await launch(url!);
+              } else {
+                throw 'cant launch $url';
+              }
+            },
+            child: Container(
+              width: screenWidth * .8,
+              height: 10,
+              padding: const EdgeInsets.all(7),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1,
+                  color: Colors.amber.withOpacity(0.3),
+                ),
+                borderRadius: BorderRadius.circular(17),
               ),
-              borderRadius: BorderRadius.circular(17),
-            ),
-            child: Column(
-              children: [
-                RichText(
-                  textScaleFactor: 1.3,
-                  text: TextSpan(
-                    text: '$projectTitle',
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () async {
-                        log('click');
-                        if (await canLaunch(url!)) {
-                          await launch(url!);
-                        } else {
-                          throw 'cant launch $url';
-                        }
-                      },
-                    style: GoogleFonts.secularOne(
-                      color: const Color(0xff1CDEC0),
+              child: Column(
+                children: [
+                  RichText(
+                    textScaleFactor: 1.3,
+                    text: TextSpan(
+                      text: '$projectTitle',
+                      recognizer: TapGestureRecognizer(),
+                      // ..onTap = () async {
+                      //   log('click');
+                      //   if (await canLaunch(url!)) {
+                      //     await launch(url!);
+                      //   } else {
+                      //     throw 'cant launch $url';
+                      //   }
+                      // },
+                      style: GoogleFonts.secularOne(
+                        color: const Color(0xff1CDEC0),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  projectDescription ?? ' ',
-                  style: GoogleFonts.ibmPlexMono(
-                    color: Colors.white60,
+                  const SizedBox(
+                    height: 4,
                   ),
-                ),
-              ],
+                  Text(
+                    projectDescription ?? ' ',
+                    style: GoogleFonts.ibmPlexMono(
+                      color: Colors.white60,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
